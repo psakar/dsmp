830ffef4b6563f45e2de8c60823613aa72c3003f
Petr Sakař <psakar@redhat.com>
2019-01-18 22:50:59 +0100

    release version 1.2

--- src/main/java/de/pdark/dsmp/Main.java
+++ src/main/java/de/pdark/dsmp/Main.java
@@ -30,7 +30,7 @@ import java.util.Set;
 public class Main
 {
     public static final Logger log = LogManager.getLogger(Main.class);
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
     
     public static void main (String[] args)
     {
